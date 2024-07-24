5adfdb45b2f75ece87faf315fd185abbbcd71a0b
akinloluwami <akinkunmioye42@gmail.com>
2024-07-24 09:46:57 +0100

    update cli.ts

--- cli.ts
+++ cli.ts
@@ -1,61 +1,12 @@
 #!/usr/bin/env node
 
-import * as fs from "fs";
-import * as path from "path";
-import * as color from "ansi-colors";
 import { promptUser } from "./prompts/promptUser";
-import { execCommand } from "./utils/execCommand";
-import { createDestinoConfig } from "./config/createDestinoConfig";
-import { createPackageJson } from "./config/createPackageJson";
-import { createTsConfig } from "./config/createTsConfig";
-import { createExampleRoutes } from "./files/createExampleRoutes";
-import { createGitIgnore } from "./files/createGitIgnore";
-import { confirm } from "@inquirer/prompts";
+import { createProject } from "./utils/createProject";
+import * as color from "ansi-colors";
 
-const main = async () => {
-  console.log(color.green("Welcome to create-destino!"));
-
-  const defaultName = process.argv[2] ?? "";
-
+(async () => {
+  const defaultName = process.argv[2];
   const config = await promptUser(defaultName);
-
-  if (!fs.existsSync(config.name)) {
-    fs.mkdirSync(config.name);
-  } else {
-    console.error(
-      color.red("Directory already exists. Please choose another name.")
-    );
-    process.exit(1);
-  }
-
-  if (config.language === "TypeScript") {
-    createTsConfig(config);
-  }
-
-  createDestinoConfig(config);
-  createPackageJson(config);
-  createExampleRoutes(config);
-  createGitIgnore();
-
-  const packageManager = config.packageManager || "npm";
-  const command = `${packageManager} install`;
-
-  const installDependencies = await confirm({
-    message: "Do you want to install dependencies?",
-    default: true,
-  });
-
-  if (installDependencies) {
-    console.log(
-      color.green(`Installing dependencies using ${packageManager}...`)
-    );
-    await execCommand(`cd ${config.name} && ${command}`);
-  }
-
-  console.log(color.green("Done. Keep coding and fly on."));
-};
-
-main().catch((error) => {
-  console.error(color.red(error));
-  process.exit(1);
-});
+  await createProject(config);
+  console.log(color.bgBlueBright("Done. Keep coding and fly on!"));
+})();

6ff472b8feaa0b42ee3e3a720f73251a38ee3211
akinloluwami <akinkunmioye42@gmail.com>
2024-07-28 12:56:59 +0100

    added cool big text

--- cli.ts
+++ cli.ts
@@ -2,11 +2,21 @@
 
 import { promptUser } from "./prompts/promptUser";
 import { createProject } from "./utils/createProject";
-import * as color from "ansi-colors";
+import figlet from "figlet";
+import color from "ansi-colors";
+
+function renderBigBoldText(text: string) {
+  return color.bold(figlet.textSync(text));
+}
 
 (async () => {
   const defaultName = process.argv[2];
+  const header = renderBigBoldText("create-destino");
+  console.log(header);
+  console.log("Let's get started!");
+
   const config = await promptUser(defaultName);
   await createProject(config);
-  console.log(color.bgBlueBright("Done. Keep coding and fly on!"));
+
+  console.log(color.bgBlueBright.black("Done. Keep coding and fly on!"));
 })();
